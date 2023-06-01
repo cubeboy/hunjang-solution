72e4f662ef94bcf79db1bb9040e35419f4c8e025
KIM JIN <cubeboy.kim@gmail.com>
2023-06-01 11:55:13 +0900

    init backend kotlin project

---
+++ hunjang/src/test/kotlin/com/clouflake/hunjang/HunjangApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clouflake.hunjang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HunjangApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
