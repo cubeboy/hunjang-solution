d8d6a2d140ff9150008acc5f2dd82107b8442fdc
cubeboy <cubeboy.kim@gmail.com>
2023-06-09 11:16:05 +0900

    String selection improvements

--- front-end/src/views/HomeView.vue
+++ front-end/src/views/HomeView.vue
@@ -70,52 +70,21 @@ export default {
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12">
-        <v-alert
-          v-show="errorMessage"
-          class="pa-4 ma-4"
-          color="warning"
-          icon="$warning"
-          title="Translate Fail"
-          :text="errorMessage"
-        ></v-alert>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-btn-toggle color="primary" v-model="actionMode" dense>
-          <v-btn>문장해석</v-btn>
-          <v-btn>단어해석</v-btn>
-        </v-btn-toggle>
-      </v-col>
-    </v-row>
-    <v-row>
       <v-col cols="12" lg="6">
         <v-sheet>
-          <v-card color="grey-lighten-4">
+          <v-card color="grey-lighten-3">
             <v-card-title>English</v-card-title>
-            <v-list>
-              <v-list-item
-                v-if="actionMode == 0"
-                class="pointer"
-                v-for="(item, index) in engTexts"
-                :key="index"
-                @click="transText(item)"
-              >
-                {{ item }}
-              </v-list-item>
-              <v-listitem v-if="actionMode == 1" v-for="(item, index) in engTexts" :key="index">
-                <section>
-                  <span
-                    class="pointer"
+              <v-card-text v-for="(item, index) in engTexts" :key="index">
+                <v-btn color="bule" icon="mdi-view-headline" size="x-small" @click="transText(item)" />
+                <span
+                    class="pointer text-body-1"
                     v-for="(word, idx) in splitText(item)"
                     :key="idx"
                     @click="transWord(word)"
                     >{{ word + ' ' }}</span
                   >
-                </section>
-              </v-listitem>
-            </v-list>
+              </v-card-text>
+            
           </v-card>
         </v-sheet>
       </v-col>
